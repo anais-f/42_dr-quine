4c4d981debaea23350bae859d69cabcd1e619432
anfichet <anfichet@student.42lyon.fr>
2026-05-26 15:21:41 +0200

    wip: error with new function

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*Comment out of main function*/
+void print() {
+	printf("Hello World!");
+}
+
 int main() {
-	char *code = "#include <stdio.h>%c%cint main() {%c%cchar *code = %c%s%c;%c%cprintf(code, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10);%c%creturn 0;%c}";
-	printf(code, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10);
+	char *code = "#include <stdio.h>%c%c/*Comment out of main function*/%cvoid print() {%c%cprintf(%cHello World!%c);%c}%c%cint main() {%c%cchar *code = %c%s%c;%c%c/* Comment in the main function*/%c%cprint();%c%cprintf(code, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10);%c%creturn 0;%c}";
+	/* Comment in the main function*/
+	print();
+	printf(code, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10);
 	return 0;
 }
