4d769bb3dfe66001078fdd424e78ced67ded53dc
anfichet <anfichet@student.42lyon.fr>
2026-05-26 15:30:13 +0200

    add: new function in colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/*Comment out of main function*/
-void print() {
-	printf("Hello World!");
+void foo() {
+	return;
 }
 
+/*Comment out of main function*/
 int main() {
-	char *code = "#include <stdio.h>%c%c/*Comment out of main function*/%cvoid print() {%c%cprintf(%cHello World!%c);%c}%c%cint main() {%c%cchar *code = %c%s%c;%c%c/* Comment in the main function*/%c%cprint();%c%cprintf(code, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10);%c%creturn 0;%c}";
+	char *code = "#include <stdio.h>%c%cvoid foo() {%c%creturn;%c}%c%c/*Comment out of main function*/%cint main() {%c%cchar *code = %c%s%c;%c%c/* Comment in the main function*/%c%cprintf(code, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10);%c%cfoo();%c%creturn 0;%c}";
 	/* Comment in the main function*/
-	print();
-	printf(code, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10);
+	printf(code, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10);
+	foo();
 	return 0;
 }
