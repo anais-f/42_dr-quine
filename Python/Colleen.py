781ec3a7211db8d64e8df1218f64e382b1087069
anfichet <anfichet@student.42lyon.fr>
2026-05-26 18:33:41 +0200

    add: python colleen

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -1,7 +1,11 @@
 # First comment
+def foo():
+	return
+
 def main():
-	s = '# First comment\ndef main():\n\ts = {0!r}\n\t# Second comment\n\tprint(s.format(s))\n\nif __name__ == "__main__":\tmain()\n'
+	s = '# First comment\ndef foo():\n\treturn\n\ndef main():\n\ts = {0!r}\n\t# Second comment\n\tprint(s.format(s))\n\tfoo()\n\nif __name__ == "__main__":\tmain()'
 	# Second comment
 	print(s.format(s))
+	foo()
 
 if __name__ == "__main__":	main()
