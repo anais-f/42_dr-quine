0662f3ec72649e9e963be0828ac0a09f149f906e
anfichet <anfichet@student.42lyon.fr>
2026-05-27 16:38:53 +0200

    feat: refactor Grace.rs to use macros for file name and code generation

--- Rust/Grace.rs
+++ Rust/Grace.rs
@@ -1,17 +1,32 @@
 use std::fs::File;
 use std::io::Write;
 
-fn main() {
-    let file_name = "Grace_kid.rs";
-    let code = r#"use std::fs::File;
+// Comment
+macro_rules! FILE_NAME {() => {"Grace_kid.rs"}}
+macro_rules! CODE {() => {r#"use std::fs::File;
 use std::io::Write;
 
-fn main() {
-    let file_name = "Grace_kid.rs";
-    let code = r$"?"$;
-    let mut file = File::create(file_name).unwrap();
-    file.write_all(code.replace('\u{24}', "$").replace('\u{3f}', code).as_bytes()).unwrap();
-}"#;
-    let mut file = File::create(file_name).unwrap();
-    file.write_all(code.replace('\u{24}', "#").replace('\u{3f}', code).as_bytes()).unwrap();
-}
+// Comment
+macro_rules! FILE_NAME {() => {"Grace_kid.rs"}}
+macro_rules! CODE {() => {r$"?"$}}
+macro_rules! MAIN {() => {
+    fn main() {
+		let file_name = FILE_NAME!();
+		let code = CODE!();
+		let mut file = File::create(file_name).unwrap();
+		file.write_all(code.replace('\u{24}', "$").replace('\u{3f}', code).as_bytes()).unwrap();
+    } 
+}}
+
+MAIN!();
+"#}}
+macro_rules! MAIN {() => {
+    fn main() {
+		let file_name = FILE_NAME!();
+		let code = CODE!();
+		let mut file = File::create(file_name).unwrap();
+		file.write_all(code.replace('\u{24}', "#").replace('\u{3f}', code).as_bytes()).unwrap();
+    } 
+}}
+
+MAIN!();
