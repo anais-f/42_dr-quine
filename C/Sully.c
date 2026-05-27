9432c9e228875d2d1dd70d302530af2cda993fb3
anfichet <anfichet@student.42lyon.fr>
2026-05-27 19:30:11 +0200

    feat: update Sully.c to improve code generation logic and simplify file handling

--- C/Sully.c
+++ C/Sully.c
@@ -4,19 +4,16 @@
 int main() {
 	int i = 5;
 	char filename[12];
-	char *code = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main() {%1$c	int i = %4$d;%1$c	char filename[12];%1$c	char *code = %2$c%3$s%2$c;%1$cif (i>=0) {%1$c		sprintf(filename, %2$cSully_%5$cd.c%2$c, i);%1$c		FILE *file = fopen(filename, %2$cw%2$c);%1$c		if (file != NULL) {%1$c			fprintf(file, code, 10, 34, code, i, 37);%1$c			fclose(file);%1$c			if (i >0) {%1$c				i--;%1$c				char command[50];%1$c				sprintf(command, %2$cgcc Sully_%5$cd.c -o Sully_%5$cd && ./Sully_%5$cd%2$c, i+1, i+1, i+1);%1$c				system(command);%1$c			}%1$c		}%1$c	}%1$c}";
-	if (i >= 0) {
-		sprintf(filename, "Sully_%d.c", i);
+	char *code = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main() {%1$c	int i = %4$d;%1$c	char filename[12];%1$c	char *code = %2$c%3$s%2$c;%1$c	if (i > 0) {%1$c		sprintf(filename, %2$cSully_%5$cd.c%2$c, i - 1);%1$c		FILE *file = fopen(filename, %2$cw%2$c);%1$c		if (file != NULL) {%1$c			fprintf(file, code, 10, 34, code, i - 1, 37);%1$c			fclose(file);%1$c			char command[50];%1$c			sprintf(command, %2$cgcc Sully_%5$cd.c -o Sully_%5$cd && ./Sully_%5$cd%2$c, i - 1, i - 1, i - 1);%1$c			system(command);%1$c		}%1$c	}%1$c}";
+	if (i > 0) {
+		sprintf(filename, "Sully_%d.c", i - 1);
 		FILE *file = fopen(filename, "w");
 		if (file != NULL) {
-			fprintf(file, code, 10, 34, code, i, 37);
+			fprintf(file, code, 10, 34, code, i - 1, 37);
 			fclose(file);
-			if (i > 0) {
-				i--;
-				char command[50];
-				sprintf(command, "gcc Sully_%d.c -o Sully_%d && ./Sully_%d", i+1, i+1, i+1);
-				system(command);
-			}
+			char command[50];
+			sprintf(command, "gcc Sully_%d.c -o Sully_%d && ./Sully_%d", i - 1, i - 1, i - 1);
+			system(command);
 		}
 	}
 }
