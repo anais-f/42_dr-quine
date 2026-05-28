54568355bc2bd8d7996da0c7f940a2cb00fa53dd
anfichet <anfichet@student.42lyon.fr>
2026-05-29 01:11:50 +0200

    feat: update Colleen.s to hexa value instead decimal

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -4,7 +4,7 @@ extern printf
 
 ; Comment out of main
 section .rodata
-	code db "default rel%1$cglobal main%1$cextern printf%1$c%1$c; Comment out of main%1$csection .rodata%1$c	code db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c	ret%1$c%1$cmain:%1$c	push rbp%1$c	mov rbp, rsp%1$c	; Comment in main%1$c%1$c	lea rdi, [code]%1$c	mov rsi, 10%1$c	mov rdx, 34%1$c	lea rcx, [code]%1$c%1$c	xor rax, rax%1$c%1$c	call printf wrt ..plt%1$c	call foo%1$c	xor rax, rax%1$c	pop rbp%1$c	ret", 0
+	code db "default rel%1$cglobal main%1$cextern printf%1$c%1$c; Comment out of main%1$csection .rodata%1$c	code db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c	ret%1$c%1$cmain:%1$c	push rbp%1$c	mov rbp, rsp%1$c	; Comment in main%1$c%1$c	lea rdi, [code]%1$c	mov rsi, 0x0A%1$c	mov rdx, 0x22%1$c	lea rcx, [code]%1$c%1$c	xor rax, rax%1$c%1$c	call printf wrt ..plt%1$c	call foo%1$c	xor rax, rax%1$c	pop rbp%1$c	ret", 0
 
 section .text
 foo:
@@ -16,8 +16,8 @@ main:
 	; Comment in main
 
 	lea rdi, [code]
-	mov rsi, 10
-	mov rdx, 34
+	mov rsi, 0x0A
+	mov rdx, 0x22
 	lea rcx, [code]
 
 	xor rax, rax
