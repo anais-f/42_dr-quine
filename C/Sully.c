ada685ccf7b01d3db5da800f847e7ee2c50f4f40
anfichet <anfichet@student.42lyon.fr>
2026-06-11 16:44:47 +0200

    fix: update .gitignore to include Sully* for wildcard matching

--- C/Sully.c
+++ C/Sully.c
@@ -5,7 +5,8 @@
 int main() {
 	int i = 5;
 	char filename[12];
-	char *code = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$cint main() {%1$c	int i = %4$d;%1$c	char filename[12];%1$c	char *code = %2$c%3$s%2$c;%1$c	if (strstr(__FILE__, %2$c_%2$c) == NULL) i = 5;%1$c	else i--;%1$c	if (i >= 0) {%1$c		sprintf(filename, %2$cSully_%5$cd.c%2$c, i);%1$c		FILE *file = fopen(filename, %2$cw%2$c);%1$c		if (file != NULL) {%1$c			fprintf(file, code, 10, 34, code, i, 37);%1$c			fclose(file);%1$c			char command[50];%1$c			sprintf(command, %2$cgcc Sully_%5$cd.c -o Sully_%5$cd && ./Sully_%5$cd%2$c, i, i, i);%1$c			system(command);%1$c		}%1$c	}%1$c}";
+	char *code = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$cint main() {%1$c	int i = %4$d;%1$c	char filename[12];%1$c	char *code = %2$c%3$s%2$c;%1$c	if (i < 0) return 1;%1$c	if (strstr(__FILE__, %2$c_%2$c) == NULL) i = 5;%1$c	else i--;%1$c	if (i >= 0) {%1$c		sprintf(filename, %2$cSully_%5$cd.c%2$c, i);%1$c		FILE *file = fopen(filename, %2$cw%2$c);%1$c		if (file != NULL) {%1$c			fprintf(file, code, 10, 34, code, i, 37);%1$c			fclose(file);%1$c			char command[50];%1$c			sprintf(command, %2$cgcc Sully_%5$cd.c -o Sully_%5$cd && ./Sully_%5$cd%2$c, i, i, i);%1$c			system(command);%1$c		}%1$c	}%1$c}";
+	if (i < 0) return 1;
 	if (strstr(__FILE__, "_") == NULL) i = 5;
 	else i--;
 	if (i >= 0) {
